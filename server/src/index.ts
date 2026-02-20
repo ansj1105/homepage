c5a7aff5b2528352c00ffb4aa6eabb499fddf5e9
An Seojung <your.email@example.com>
2026-02-20 15:32:47 +0900

    asfa

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { createApp } from "./app";
 import { closePool, ensureConnection, seedDatabaseIfNeeded } from "./db";
 
-const port = Number(process.env.PORT ?? 8787);
+const port = Number(process.env.PORT ?? 3000);
 const app = createApp();
 
 const bootstrap = async () => {
